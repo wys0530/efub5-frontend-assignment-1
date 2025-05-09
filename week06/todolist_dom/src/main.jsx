7c68c7b2c8195edc0a6abb0bd0bdab0837282e92
wys0530 <166965895+wys0530@users.noreply.github.com>
2025-05-10 04:19:11 +0900

    feat: 6주차 과제 제출

---
+++ week06/todolist_dom/src/main.jsx
@@ -0,0 +1,17 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Diary from "./components/Diary.jsx";
+
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/diary" element={<Diary />} />
+      </Routes>
+    </BrowserRouter>
+  </StrictMode>
+);
